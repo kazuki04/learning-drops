d0b48dc86f239eba8c0a7ee70b4282ff1f52ea5f
kazuki04 <kazukik0402@gmail.com>
2021-10-11 20:30:27 +0900

    add icon to sidebar and modify

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -5,14 +5,12 @@ import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import SearchInput from "./SearchInput"
 
-const drawerWidth = 240;
-
 const Header = () => {
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <CssBaseline />
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h6" noWrap component="div" sx={{ p: 2 }}>
             Learning Drops
           </Typography>
         </Toolbar>
