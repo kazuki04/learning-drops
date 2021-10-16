11bdf1cdb45b7588128d83214e556e3246b5ae8e
kazuki04 <kazukik0402@gmail.com>
2021-10-16 19:59:52 +0900

    wrap App component with an Auth0Provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import auth_config from "../auth_config.json"
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Auth0Provider
+    domain={auth_config.domain}
+    clientId={auth_config.clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
