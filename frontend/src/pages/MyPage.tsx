78b1da9ac01bba84392375a3b1da0f8477d877ce
kazuki04 <kazukik0402@gmail.com>
2021-10-16 21:14:15 +0900

    modfiy directory

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Box from '@mui/material/Box';
-import Header from "../components/layouts/Header";
-import SideBar from "../components/layouts/Sidebar";
+import Header from "../components/organisms/Header";
+import SideBar from "../components/organisms/SideBar";
 
 const MyPage = () => {
   return (
