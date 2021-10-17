ce48e931b7a9752d2c088a982f8fa4d4d3883c20
kazuki04 <kazukik0402@gmail.com>
2021-10-17 15:19:26 +0900

    wrap root component with ThemeProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import auth_config from "../auth_config.json"
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from "./components/atoms/color/Theme"
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={auth_config.domain}
-    clientId={auth_config.clientId}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  <ThemeProvider theme={theme}>
+    <Auth0Provider
+      domain={auth_config.domain}
+      clientId={auth_config.clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </ThemeProvider>
+  ,
   document.getElementById("root")
 );
