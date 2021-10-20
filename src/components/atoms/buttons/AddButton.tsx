4ebfcfd93e596ee868897996765b164fc4967325
kazuki04 <kazukik0402@gmail.com>
2021-10-21 00:11:14 +0900

    create AddButton.tsx

---
+++ src/components/atoms/buttons/AddButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+const AddButton = () =>  {
+  return (
+    <React.Fragment>
+      <AddCircleOutlineIcon />
+    </React.Fragment>
+  );
+}
+
+export default AddButton;
