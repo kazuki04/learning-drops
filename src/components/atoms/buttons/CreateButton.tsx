840db9ce15d1548dde0135a259d60e7d9b2b6027
kazuki04 <kazukik0402@gmail.com>
2021-10-21 21:35:40 +0900

    create CreateButton.tsx

---
+++ src/components/atoms/buttons/CreateButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+const CreateButton = () => {
+  return (
+    <Button>Create</Button>
+  )
+};
+
+export default CreateButton;
