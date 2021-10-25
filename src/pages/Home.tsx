1d370fc4c267fece38c589fadc4f5287dd0e14fc
kazuki04 <kazukik0402@gmail.com>
2021-10-25 21:50:31 +0900

    rename Home.js

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Header from "../components/organisms/Header";
+import SideBar from "../components/organisms/SideBar";
+
+const Home = () => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Header />
+      <SideBar />
+    </Box>
+    )
+} 
+
+export default Home;
