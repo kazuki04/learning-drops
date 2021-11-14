901f180ce4d44675a042c8895dc8c17a3e555ebd
kazuki04 <kazukik0402@gmail.com>
2021-11-14 17:31:00 +0900

    modify create button

--- src/components/atoms/buttons/CreateButton.tsx
+++ src/components/atoms/buttons/CreateButton.tsx
@@ -3,11 +3,12 @@ import Button from '@mui/material/Button';
 
 interface CreateButtonProps {
   doRequest?: () => void
+  button_word?: string
 }
 
 const CreateButton = (prop: CreateButtonProps) => {
   return (
-    <Button onClick={prop.doRequest}>Create</Button>
+    <Button onClick={prop.doRequest}>Create {prop.button_word}</Button>
   )
 };
 
