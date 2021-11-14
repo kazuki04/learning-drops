504e38a10333add5615ad42c10ee39b2cc2c7204
kazuki04 <kazukik0402@gmail.com>
2021-11-14 16:24:35 +0900

    modify api types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,10 +1,15 @@
 export interface Request {
   request_url: string,
-  payload?: Section,
+  // To Do: The payload's type is appropriate?
+  // Should I set the type AnyJson?
+  payload?: Object,
   accessToken?: Promise<string> | undefined
 }
 
-export interface Section {
-  title: string
-  user_id: string
-} 
+export interface Response {
+  data: AnyJson
+}
+
+type AnyJson =  boolean | number | string | null | JsonArray | JsonMap;
+interface JsonMap {  [key: string]: AnyJson; }
+interface JsonArray extends Array<AnyJson> {}
