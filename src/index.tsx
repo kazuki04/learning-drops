a901f9328fe88593c716642edb5ee674e485d743
kazuki04 <kazukik0402@gmail.com>
2021-11-14 17:37:19 +0900

    wrap the App Component with Provider Component for Redux

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,21 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import auth_config from "../auth_config.json"
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from "./components/atoms/color/Theme"
+import store from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Auth0Provider
-      domain={auth_config.domain}
-      clientId={auth_config.clientId}
-      redirectUri={window.location.origin}
-      audience={auth_config.audience}
-    >
-      <App />
-    </Auth0Provider>
+    <Provider store={store}>
+      <Auth0Provider
+        domain={auth_config.domain}
+        clientId={auth_config.clientId}
+        redirectUri={window.location.origin}
+        audience={auth_config.audience}
+      >
+        <App />
+      </Auth0Provider>
+    </Provider>
   </ThemeProvider>
   ,
   document.getElementById("root")
