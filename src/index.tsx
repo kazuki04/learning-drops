3687e5168e12fe68267f980ef63335ae1c26983e
kazuki04 <kazukik0402@gmail.com>
2021-11-28 20:36:56 +0900

    remove redux and saga packages

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,17 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import auth_config from "../auth_config.json"
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from "./components/atoms/color/Theme"
-import store from './store'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <Auth0Provider
-        domain={auth_config.domain}
-        clientId={auth_config.clientId}
-        redirectUri={window.location.origin}
-        audience={auth_config.audience}
-      >
-        <App />
-      </Auth0Provider>
-    </Provider>
+    <Auth0Provider
+      domain={auth_config.domain}
+      clientId={auth_config.clientId}
+      redirectUri={window.location.origin}
+      audience={auth_config.audience}
+    >
+      <App />
+    </Auth0Provider>
   </ThemeProvider>
   ,
   document.getElementById("root")
