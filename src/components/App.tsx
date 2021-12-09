1c52715bac0fabd389bfe1bd0de8c1f0058927fc
kazuki04 <kazukik0402@gmail.com>
2021-12-09 19:46:55 +0900

    add header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Routes from "./routes/index"
+import Header from "../components/molecules/header/Header"
 import { useAuth0 } from "@auth0/auth0-react";
 import LinearIndeterminate from "../components/atoms/progress/LinearProgress";
 
 const App = () => {
-  const { isLoading } = useAuth0();
+  const { isLoading, isAuthenticated } = useAuth0();
+
 
   if (isLoading) {
     return (
       <LinearIndeterminate />
+    )
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <Header />
     )
   }
 
